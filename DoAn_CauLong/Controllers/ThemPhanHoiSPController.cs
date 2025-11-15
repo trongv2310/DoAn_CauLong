4e6d7c6c30a4dd8253290964f1450586afeff51d
Vi Do <vidotrong@gmail.com>
2025-11-15 19:07:27 +0700

    firts commit

---
+++ DoAn_CauLong/Controllers/ThemPhanHoiSPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_CauLong.Controllers
+{
+    public class ThemPhanHoiSPController : Controller
+    {
+        // GET: ThemPhanHoiSP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

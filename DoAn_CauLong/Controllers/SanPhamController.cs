42d3452eb8736fd8323c5f996f83c7e4ca5096a3
Ho Si Khai <khaiho150405@gmail.com>
2025-11-15 17:25:50 +0700

    chitietsp

--- DoAn_CauLong/Controllers/SanPhamController.cs
+++ DoAn_CauLong/Controllers/SanPhamController.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using DoAn_CauLong.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace DoAn_CauLong.Controllers
 {
     public class SanPhamController : Controller
     {
+        private Model1 data = new Model1();
         // GET: SanPham
-        public ActionResult Index()
+        public ActionResult ViewAllProduct(int maLoai)
         {
-            return View();
+            // 1. Lấy tên loại sản phẩm để hiển thị trên tiêu đề trang
+            var loaiSanPham = data.LoaiSanPhams
+                .SingleOrDefault(l => l.MaLoai == maLoai);
+
+            if (loaiSanPham == null)
+            {
+                return HttpNotFound();
+            }
+
+            // 2. Lấy tất cả sản phẩm thuộc loại này
+            var sanPhams = data.SanPhams
+                .Where(sp => sp.MaLoai == maLoai)
+                .Include(sp => sp.LoaiSanPham) // Tùy chọn: Bao gồm thông tin loại nếu cần
+                .ToList();
+
+            // 3. Truyền danh sách sản phẩm và tên loại vào View
+            ViewBag.TenLoai = loaiSanPham.TenLoai;
+            return View(sanPhams);
         }
     }
 }
